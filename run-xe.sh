0189f0585bd2f1302f05a0c15be650d035cfcad3
Yusuf Gündüz <yusuf.gunduz@epias.com.tr>
2017-07-14 15:24:13 +0300

    initial commit

---
+++ run-xe.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo docker run --name docker-oracle \
+--shm-size=1g \
+-p 1521:1521 -p 48080:8080 \
+-e ORACLE_PWD=yg1234565 \
+-v /$(pwd)/src:/u01/app/oracle/oradata \
+oracle/database:11.2.0.2-xe
