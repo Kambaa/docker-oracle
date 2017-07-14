844bdb3305eb399e6be3968f247019567db1ce9b
Yusuf Gündüz <Kambaa@users.noreply.github.com>
2017-07-14 15:34:36 +0300

    Update run-xe.sh

--- run-xe.sh
+++ run-xe.sh
@@ -3,6 +3,6 @@
 sudo docker run --name docker-oracle \
 --shm-size=1g \
 -p 1521:1521 -p 48080:8080 \
--e ORACLE_PWD=yg1234565 \
+-e ORACLE_PWD=1234565 \
 -v /$(pwd)/src:/u01/app/oracle/oradata \
 oracle/database:11.2.0.2-xe
